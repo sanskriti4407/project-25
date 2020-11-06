081ab4af6bc2081785f007e8b43c8db1b9909fa3
sanskriti4407 <71079762+sanskriti4407@users.noreply.github.com>
2020-11-06 19:42:15 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,30 @@
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			'isStatic':false,
+			'restitution':0.5,
+			'friction':3,
+			'density':1.2
+   }
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("paper.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display(){
+		var pos =this.body.position;
+		var angle = this.body.angle;
+		push();
+		translate(pos.x, pos.y);
+		rotate(angle);
+		imageMode(CENTER);
+			image(this.image, 0,0,this.r, this.r)
+		pop();
+	  }
+	
+}
